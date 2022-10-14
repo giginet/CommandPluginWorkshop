ac40701e905af98f4f41fc04e8ddf56fa61df47d
giginet <giginet.net@gmail.com>
2022-10-14 14:56:39 +0900

    Initial Commit

---
+++ CommandPluginWorkshop/CommandPluginWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommandPluginWorkshopApp.swift
+//  CommandPluginWorkshop
+//
+//  Created by giginet on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct CommandPluginWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
